84320da0e6a9e863979982e1e3edcb1e1b7d90bc
Manuareraa <manuareraa@gmail.com>
2022-01-08 15:15:28 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,8 @@
+let data = {
+    assetMinterAddress: '0xe084fE65De082F249A26Ae1d80646a26D4bb2174',
+    gpiAddress: '0x8Ee7484a06041288ab98A792C5cD1C3716951299',
+    idCardFee: "1415",
+    backendServer: "http://localhost:5000/"
+}
+
+export default data;
