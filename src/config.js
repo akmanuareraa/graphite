718a8d4ed70a1aeb05a1263fe50faf5ea3bbfef5
Manuareraa <manuareraa@gmail.com>
2022-01-09 22:25:00 +0530

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 let data = {
-    assetMinterAddress: '0xe084fE65De082F249A26Ae1d80646a26D4bb2174',
+    assetMinterAddress: '0x48376676392965112aBf4a6fF860481C6CdB5446',
     gpiAddress: '0x8Ee7484a06041288ab98A792C5cD1C3716951299',
-    idCardFee: "1415",
-    backendServer: "http://localhost:5000/"
+    idCardFee: "1412",
+    //backendServer: "http://34.93.254.194:5000/",
+    backendServer: "http://localhost:5000/",
+    redirectUrl: "https:///www.ezofis.com/Products.html?"
 }
 
 export default data;
