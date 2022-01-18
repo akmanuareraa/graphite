3cc99fd8853088645e29e47cbbaa751374838884
Manuareraa <manuareraa@gmail.com>
2022-01-18 14:26:10 +0530

    Add files via upload

--- Backend/Backend v2.js
+++ Backend/Backend v2.js
@@ -205,9 +205,16 @@ async function addNewId(JSdoc) {
     }
 }
 
+
 app.get("/getIdCard", function (req, res) {
+    let param = null
+    if (Object.keys(req.body).length !== 0) {
+        param = req.body
+    } else {
+        param = req.query
+    }
     console.log("Reached here", req.body);
-    getIdCard(req.body).then(function (response, error) {
+    getIdCard(param).then(function (response, error) {
         if (response) {
             console.log(response)
             if (response === 'No Record Found') {
@@ -237,11 +244,90 @@ async function getIdCard(JSdoc) {
     }
 }
 
+app.get("/addVerifyToken", function (req, res) {
+    idAssetMinterContract.methods.addVerifyToken(req.body.token, req.body.docno).send({ from: config.owner })
+        .on('transactionHash', function (hash) {
+            console.log(hash)
+        })
+        .on('receipt', function (receipt) {
+            console.log(receipt)
+        })
+        .on('confirmation', function (confirmationNumber, receipt) {
+            if (confirmationNumber == 2) {
+                console.log(confirmationNumber, receipt)
+                res.status(200).json({ "Message": "Successfully added", "Transaction Hash": receipt.transactionHash })
+            }
+        })
+        .on('error', function (error, receipt) {
+            console.log({ "Message": "Failed", "Error": error })
+        })
+})
+
+app.get("/getIdCardVerify", function (req, res) {
+    console.log("Reached here", req.query);
+    getIdCardVerify(req.query).then(function (response, error) {
+        if (response) {
+            console.log(response)
+            if (response == 'No Record Found') {
+                res.status(404).json("No Record Found")
+            } else {
+                res.status(200).json({ ID: response })
+            }
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+// reuse the getIdCard Function
+async function getIdCardVerify(JSdoc) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const coll = database.collection("idcard");
+        const Cursor = await coll.findOne({ "docno": JSdoc.docno });
+        console.log('Cursor: ', Cursor)
+        if (Cursor) {
+            return Cursor;
+        } else {
+            return 'No Record Found';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.post("/addSalesOrder", function (req, res) {
+    console.log("Reached here", req.body);
+    addSalesOrder(req.body).then(function (response, error) {
+        if (response) {
+            console.log(response)
+            res.status(200).json({ "New Sales Order Added": response });
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function addSalesOrder(JSdoc) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const coll = database.collection("salesOrder");
+        const Cursor = await coll.insertOne(
+            { ...JSdoc }
+        );
+        return Cursor;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 app.get("/getSalesOrder", function (req, res) {
     console.log("Reached here", req.query);
     getSalesOrder(req.query).then(function (response, error) {
         if (response) {
-            console.log(response)
+            console.log('RESPONSE: ',response)
             if (response === 'No Record Found') {
                 res.status(200).json("No Record Found")
             } else {
@@ -266,120 +352,6 @@ async function getSalesOrder(JSdoc) {
             } else {
                 return 'No Record Found';
             }
-        } else {
-            return 'No Record Found';
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-app.get("/addVerifyToken", function (req, res) {
-    idAssetMinterContract.methods.addVerifyToken(req.body.token, req.body.docno).send({ from: config.owner })
-        .on('transactionHash', function (hash) {
-            console.log(hash)
-        })
-        .on('receipt', function (receipt) {
-            console.log(receipt)
-        })
-        .on('confirmation', function (confirmationNumber, receipt) {
-            if (confirmationNumber == 2) {
-                console.log(confirmationNumber, receipt)
-                res.status(200).json({ "Message": "Successfully added", "Transaction Hash": receipt.transactionHash })
-            }
-        })
-        .on('error', function (error, receipt) {
-            console.log({ "Message": "Failed", "Error": error })
-        })
-})
-
-app.get("/verifyId", function (req, res) {
-    console.log("Reached here", req.body);
-    verifyId(req.body).then(function (response, error) {
-        if (response) {
-            console.log(response)
-            if (response === 'No Record Found') {
-                res.status(404).json("No Record Found")
-            } else {
-                res.status(200).json({ "Sales Order": response })
-            }
-        } else {
-            res.status(500).json({ "Error": error })
-        }
-    })
-})
-
-async function verifyId(JSdoc) {
-    try {
-        await client.connect();
-        const database = client.db(dbName);
-        const coll = database.collection("salesOrder");
-        const Cursor = await coll.findOne({ "orderId": JSdoc.orderId });
-        console.log('Cursor: ', Cursor)
-        if (Cursor) {
-            return Cursor;
-        } else {
-            return 'No Record Found';
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-app.post("/addSalesOrder", function (req, res) {
-    console.log("Reached here", req.body);
-    addSalesOrder(req.body).then(function (response, error) {
-        if (response) {
-            console.log(response)
-            res.status(200).json({ "New Sales Order Added": response });
-        } else {
-            res.status(500).json({ "Error": error })
-        }
-    })
-})
-
-async function addSalesOrder(JSdoc) {
-    try {
-        await client.connect();
-        const database = client.db(dbName);
-        const coll = database.collection("salesOrder");
-        console.log('Found 1')
-        const Cursor = await coll.insertOne(
-            { ...JSdoc }
-        );
-        return Cursor;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-app.get("/getIdCardVerify", function (req, res) {
-    console.log("Reached here", req.query);
-    getIdCardVerify(req.query).then(function (response, error) {
-        if (response) {
-            console.log(response)
-            if (response == 'No Record Found') {
-                res.status(404).json("No Record Found")
-            } else {
-                res.status(200).json({ ID: response })
-            }
-        } else {
-            res.status(500).json({ "Error": error })
-        }
-    })
-})
-
-async function getIdCardVerify(JSdoc) {
-    try {
-        await client.connect();
-        const database = client.db(dbName);
-        const coll = database.collection("idcard");
-        console.log("ESTID: ", JSdoc.estId)
-        const Cursor = await coll.findOne({ "docno": JSdoc.docno });
-        console.log('Cursor: ', Cursor)
-        if (Cursor) {
-            return Cursor;
         } else {
             return 'No Record Found';
         }
@@ -429,3 +401,212 @@ async function getSalesOrderStatus(JSdoc) {
 }
 
 
+// invoice
+
+app.post("/addInvoice", function (req, res) {
+    console.log("Reached here", req.body);
+    addInvoice(req.body).then(function (response, error) {
+        if (response) {
+            console.log(response)
+            res.status(200).json({ "New Invoice Added": response });
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function addInvoice(JSdoc) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const coll = database.collection("invoice");
+        const Cursor = await coll.insertOne(
+            { ...JSdoc }
+        );
+        return Cursor;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get("/getInvoice", function (req, res) {
+    console.log("Reached here", req.query);
+    getInvoice(req.query).then(function (response, error) {
+        if (response) {
+            console.log('RESPONSE: ',response)
+            if (response === 'No Record Found') {
+                res.status(200).json("No Record Found")
+            } else {
+                res.status(200).json({ "Invoice": response })
+            }
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function getInvoice(JSdoc) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const coll = database.collection("invoice");
+        const Cursor = await coll.find({ "invoiceno": JSdoc.invoiceno }).project({ _id: 0 })
+        const value = await Cursor.toArray();
+        if (Cursor) {
+            if (value.length !== 0) {
+                return value[0];
+            } else {
+                return 'No Record Found';
+            }
+        } else {
+            return 'No Record Found';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get("/getInvoiceStatus", function (req, res) {
+    let param = null
+    if (Object.keys(req.body).length !== 0) {
+        param = req.body
+    } else {
+        param = req.query
+    }
+    getInvoice(param).then(function (response, error) {
+        if (response) {
+            if (response === 'No Record Found') {
+                res.status(200).json("No Record Found")
+            } else {
+                getInvoiceStatus(response).then(function (response, error) {
+                    if (response) {
+                        res.status(200).json({ "Status": response })
+                    } else {
+                        console.log(error)
+                    }
+                })
+            }
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function getInvoiceStatus(JSdoc) {
+    try {
+        let hash = Web3.utils.keccak256(JSON.stringify(JSdoc))
+        let invoiceStatus = await logisticsAssetMinterContract.methods.assetsIssued(hash).call({ from: config.owner })
+        if (invoiceStatus.customerApproval) {
+            return "Approved"
+        } else {
+            return "Not Approved"
+        }
+    } catch (error) {
+        console.log('ERROR')
+    }
+}
+
+// logistics
+
+app.post("/addLogistics", function (req, res) {
+    console.log("Reached here", req.body);
+    addLogistics(req.body).then(function (response, error) {
+        if (response) {
+            console.log(response)
+            res.status(200).json({ "New Logistics Added": response });
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function addLogistics(JSdoc) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const coll = database.collection("logistics");
+        const Cursor = await coll.insertOne(
+            { ...JSdoc }
+        );
+        return Cursor;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get("/getLogistics", function (req, res) {
+    console.log("Reached here", req.query);
+    getLogistics(req.query).then(function (response, error) {
+        if (response) {
+            console.log('RESPONSE: ',response)
+            if (response === 'No Record Found') {
+                res.status(200).json("No Record Found")
+            } else {
+                res.status(200).json({ "Logistics": response })
+            }
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function getLogistics(JSdoc) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const coll = database.collection("logistics");
+        const Cursor = await coll.find({ "cha": JSdoc.cha }).project({ _id: 0 })
+        const value = await Cursor.toArray();
+        if (Cursor) {
+            if (value.length !== 0) {
+                return value[0];
+            } else {
+                return 'No Record Found';
+            }
+        } else {
+            return 'No Record Found';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get("/getLogisticsStatus", function (req, res) {
+    let param = null
+    if (Object.keys(req.body).length !== 0) {
+        param = req.body
+    } else {
+        param = req.query
+    }
+    getLogistics(param).then(function (response, error) {
+        if (response) {
+            if (response === 'No Record Found') {
+                res.status(200).json("No Record Found")
+            } else {
+                getLogisticsStatus(response).then(function (response, error) {
+                    if (response) {
+                        res.status(200).json({ "Status": response })
+                    } else {
+                        console.log(error)
+                    }
+                })
+            }
+        } else {
+            res.status(500).json({ "Error": error })
+        }
+    })
+})
+
+async function getLogisticsStatus(JSdoc) {
+    try {
+        let hash = Web3.utils.keccak256(JSON.stringify(JSdoc))
+        let logisticsStatus = await logisticsAssetMinterContract.methods.assetsIssued(hash).call({ from: config.owner })
+        if (logisticsStatus.customerApproval) {
+            return "Approved"
+        } else {
+            return "Not Approved"
+        }
+    } catch (error) {
+        console.log('ERROR')
+    }
+}
