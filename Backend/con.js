69aa94fe05c546d6b1a96456e1e2634faa578cde
Manuareraa <manuareraa@gmail.com>
2022-04-21 12:32:47 +0530

    Add files via upload

---
+++ Backend/con.js
@@ -0,0 +1,16 @@
+//let data
+let config = {
+    idAssetMinterAddress: "0x56038BdfE1Ecb84508210719d6337691252a80b3",
+    logisticsAssetMinterAddress: "0x1f84c3dA318a5A4b34C5596C57585775A6B9215A",
+    rpc:"https://rpc-mumbai.maticvigil.com",
+    owner:"0xAb4260213f2c79F9628D47fA0906119291a3331d",
+    secret: "e923633d8436cd611d656b619a80d06b263cd9d0c8b1092bad62f626f79048d7",
+    gpiAddress: '0x8Ee7484a06041288ab98A792C5cD1C3716951299',
+    backendServer: "http://35.244.33.55:5000/",
+    //backendServer: "http://localhost:5000/",
+    redirectUrl: "https:///www.ezofis.com/Products.html?",
+    domain: "http://localhost:3000/"
+}
+
+//export default data;
+module.exports = config;
